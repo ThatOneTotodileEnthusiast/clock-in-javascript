f7bc61abfcf92c437cbde0c8b517d1e21d893cf0
hassanraza0054 <m-h-r300@hotmail.com>
2019-12-30 20:36:31 +1100

    Clock Complete!

--- commentBox.js
+++ commentBox.js
@@ -1,7 +1,22 @@
 var secondsHand = document.querySelector(".seconds");
+var clockTimer = document.querySelector("#timer");
+var time = 180;
+var timeOffset = 180;
 
 function clockLoop() {
-  setInterval(() => {
-    
-  }, 1000)
+  if (time === 540) {
+    time = 180;
+    secondsHand.style.transform = `rotate(${time + 0.1875}deg)`;
+  }
+  else {
+    secondsHand.style.transform = `rotate(${time + 0.1875}deg)`;
+  }
+  time += 0.1875;
+
+  if (time % 6 === 0) {
+    clockTimer.innerHTML = (time-timeOffset)/6;
+  }
+  console.log("Interval timer: " + time);
 }
+
+setInterval(clockLoop, 31.25)
